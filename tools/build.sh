f8ec8c1c6cb09565770a3a5ea8575733a404ab07
Adu Bhandaru <syphondu@gmail.com>
2013-11-04 03:06:51 -0500

    finished build tool

--- tools/build.sh
+++ tools/build.sh
@@ -5,4 +5,29 @@
 # Build library from source.
 
 echo ">> Building ..."
-echo "<nothing to do>"
+
+IN_DIR="src"
+OUT_DIR="build"
+FILES=("G3.js" "Class.js" "Model.js" "Light.js" "Event.js" "Events.js" "Game.js")
+MINIFIER="uglifyjs"
+OUTPUT="game3.min.js"
+
+# build the command
+command="cat "
+for file in "${FILES[@]}"
+do
+  command="${command}${IN_DIR}/${file} "
+  echo $file
+done
+
+# create build directory (if needed)
+mkdir -p "${OUT_DIR}"
+
+# minifier command
+command="${command}| ${MINIFIER} -o ${OUT_DIR}/${OUTPUT}"
+
+# run command
+echo ">> Running minifier ..."
+eval "${command}"
+
+echo ">> Done!"
